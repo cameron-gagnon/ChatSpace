4f9218bb787b83ec94e1d6636ceebf13e6f3abac
Cameron Gagnon <cgagnon@umich.edu>
2015-09-19 19:51:47 -0400

    have it dynamically populating extension

--- scripts/popup.js
+++ scripts/popup.js
@@ -18,46 +18,69 @@ function initIFrame() {
                 initHTML(xmlhttp);
             }
         }
-
-//        loginfacebook(initIFrame);
       }
   });
 }
 
 function parseHTML(html){
-    var re = new RegExp("(?:group_browse_)[0-9]*", "g");
-    var m, matches=[], num=0;    
+    // captures the group ID from facebook
+    var reid = new RegExp('(?:group_browse_)[0-9]*', 'gi');
+    // captures the title of the fb group
+    var retitle = new RegExp('class="groupsRecommendedTitle"[^>]*>[ ]*([^<]*)', 'gi');
+    var mid, mtitle, ids=[], titles=[], num=0;    
     // loop through all matches until all matches are found as indicated by
     // a false m 
     do {
-        m = re.exec(html);
+        // get the id of the groups
+        mid = reid.exec(html);
 
-        if (m){
-            num = num + 1;
-            m = String(m).slice(13);
-            console.log(m); 
-            matches.push(m);
+        if (mid){
+            // for every group, get the title in an equivalent way
+            mtitle = retitle.exec(html);
+            // strip off the beginning of group_browse_[0-9]
+            mid = String(mid).slice(13);
+            // get the title from the title regex
+            mtitle = mtitle[1];
+            // debug stuff
+            console.log(mid, mtitle); 
+            // update ids and titles parallely 
+            ids.push(mid);
+            titles.push(mtitle);
         }
 
-    } while (m);
+    } while (mid);
 
-    console.log(matches); 
+    return [ids, titles];
+}
 
-    return matches;
+function populatePopup(id, title){
+    var fb = document.createElement('div');
+    var text = document.createTextNode(title);
+    fb.id = 'groups';
+    fb.appendChild(text); // add text node
+    var parentDiv = document.getElementById('parent');
+    document.body.insertBefore(fb, parentDiv);
 }
 
 function initHTML(xmlhttp){
     var html = xmlhttp.response;
-    var groupIDs = parseHTML(html); 
+    var data = parseHTML(html); 
+    var ids = data[0];
+    var titles = data[1];
     var x;
-    for (x in groupIDs){
-        openID(groupIDs[x]);
+    var parentDiv = document.createElement('div');
+    parentDiv.id = 'parent';
+
+    for (x in ids){
+        populatePopup(ids[x], titles[x]);
     }
+    
+    var grandParent = document.getElementById('fbdata');
+    document.body.insertBefore(parentDiv, grandParent);
+
 }
 
-function openID(id){
-    window.open('http://facebook.com/groups/' + id);
-}
+
 
 document.addEventListener('DOMContentLoaded', function () {
     initIFrame();
