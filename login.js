519d23a83c3df662ef9c08a58e70f10896cabd7e
Amyxu <amy_xuanqi@hotmail.com>
2015-09-19 02:32:12 -0400

    test 1 for login

---
+++ login.js
@@ -0,0 +1,28 @@
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+  
+
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '908225099215087',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+
+FB.getLoginStatus(function(response) {
+  if (response.status === 'connected') {
+    console.log('Logged in.');
+  }
+  else {
+    FB.login();
+  }
+});
+
